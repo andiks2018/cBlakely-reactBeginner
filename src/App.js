c6d8bc7c4f45d43f31a050e9aef3b0326ef40ab2
andi <andiks2018@gmail.com>
2022-12-26 08:41:20 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,87 @@
-import React from 'react';
-import {Routes, Route} from 'react-router-dom'
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import ContactCard from "./ContactCard";
 
-function App() {
+const App = () => {
+
+  // const contacts = [
+  //   {
+  //     avatarUrl : "https://via.placeholder.com/150",
+  //     name : "andi",
+  //     email : "andi@andi.com",
+  //     age : 36
+  //   },
+  //   {
+  //     avatarUrl : "https://via.placeholder.com/150",
+  //     name : "kakak",
+  //     email : "kakak@kakak.com",
+  //     age : 10
+  //   }, 
+  //   {
+  //     avatarUrl : "https://via.placeholder.com/150",
+  //     name : "aan",
+  //     email : "aan@aan.com",
+  //     age : 8
+  //   }
+  // ]
+
+  const [results, setResults]=useState([])
+  
+  useEffect(()=>{
+    fetch("https://randomuser.me/api/?results=3")
+    .then(response=>response.json())
+    .then(data=>{
+      console.log(data)
+      setResults(data.results)
+    })
+  }, [])
+
   return (
-    <Routes>
-      <Route path='/' element={<h1>Hello</h1>} />
-    </Routes>
-  );
-}
+    <div>
 
+        {results.map((result, index)=>{
+        return(
+          <ContactCard key={index}
+          avatarUrl = {result.picture.large}
+          name = {result.name.first}
+          email = {result.email}
+          age = {result.dob.age}
+        />
+        )
+      })}
+
+
+      {/* {contacts.map((contact, index)=>{
+        return(
+          <ContactCard key={index}
+          avatarUrl = {contact.avatarUrl}
+          name = {contact.name}
+          email = {contact.email}
+          age = {contact.age}
+        />
+        )
+      })} */}
+
+
+      {/* <ContactCard 
+        avatarUrl = "https://via.placeholder.com/150"
+        name = "andi"
+        email = "andi@andi.com"
+        age = {36}
+      />
+      <ContactCard 
+      avatarUrl = "https://via.placeholder.com/150"
+      name = "kakak"
+      email = "kakak@kakak.com"
+      age = {10}
+      />
+      <ContactCard 
+      avatarUrl = "https://via.placeholder.com/150"
+      name = "aan"
+      email = "aan@aan.com"
+      age = {8}
+      /> */}
+    </div>
+  );
+};
 export default App;
